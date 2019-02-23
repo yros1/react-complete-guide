700b9ab4b261ab218612183a4f844a420f5c3fe6
AD\yros <pis2me@hotmail.com>
2019-02-23 20:46:12 +0000

    Create Person component using ES6 arrow function syntax.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 // Throug App component nest here all other components the app might need
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
         <p> This is realy working!</p>
+        <Person />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
---
+++ src/Person/Person.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// in general, a component is a fnction which returns some JSX - the custom html element.
+// 1) Simple fnction approach
+// function person(){
+//     return <h2> some jsx
+// }
+
+// 2) ES5 syntax of variable which holds a function
+// var person = function(){
+//     return <h2> some jsx
+// }
+
+// 3) ES6 syntax
+const person = () => {
+    return <p> I'm a Person!</p>
+};
+
+export default person;
