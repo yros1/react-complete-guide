06bff2524730ecacdf5b3940be2aa9a99538f912
artur.dauksza <artur.dauksza@inps.co.uk>
2019-02-25 15:51:01 +0000

    Understanding the Children Property

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -13,7 +13,12 @@ import React from 'react';
 
 // 3) ES6 syntax
 const person = (props) => {
-    return <p> I'm {props.name} and I am {props.age} years old!</p>
+    return (
+        <div>
+            <p> I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
