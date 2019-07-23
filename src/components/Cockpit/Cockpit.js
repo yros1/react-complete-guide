5727a53853f719b0a2195c4efdd5e3d7ecf26cd7
artur.dauksza <artur.dauksza@inps.co.uk>
2019-07-23 08:50:32 +0100

    Example of cleanup work for timer.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -10,11 +10,12 @@ const cockpit = (props) => {
      useEffect(() => {
        console.log('[Cockpit.js useEffect]');
        // Http request...
-       setTimeout(() => {
+       const timer = setTimeout(() => {
          alert('Saved data to cloud!');
        }, 1000);
        // Anonymous function in return runs BEFORE the main useEffect function runs, but AFTER the (first) render cycle.
        return () => {
+         clearTimeout(timer);
          console.log('[Cockpit.js cleanup work in useEffect]');
        };
      }, []); // if pass an empty array then useEffect runs just once at first time. - This technic mimic 
