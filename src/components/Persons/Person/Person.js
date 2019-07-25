77b4a380a9668604cdab59f203fe7d21d6cebf69
artur.dauksza <artur.dauksza@inps.co.uk>
2019-07-25 09:49:04 +0100

    Using Refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -17,6 +17,16 @@ import classes from './Person.css';
 
 // 3) ES6 syntax
 class Person extends Component {
+    constructor(props) {
+        super(props); // Always when you add constructor, add super first.
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         // It returns JSX code.
         console.log('[Person.js] rendering...');    
@@ -25,7 +35,14 @@ class Person extends Component {
                 <p onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old!
                 </p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input 
+                    // ref={(inputEl) => {this.inputElement = inputEl}} // old way - function approach
+                    ref = {this.inputElementRef}
+                    key="i3"
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}
+                />
             </Aux>
         );
     }    
